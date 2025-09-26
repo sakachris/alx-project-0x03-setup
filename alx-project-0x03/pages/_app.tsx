950c4be528a58bd09b231e4eaea983931276e479
sakachris <sakachris90@gmail.com>
2025-09-26 16:13:34 +0300

    Setting up our landing page with Imperative routing

--- alx-project-0x03/pages/_app.tsx
+++ alx-project-0x03/pages/_app.tsx
@@ -1,14 +1,8 @@
-// import "@/styles/globals.css";
-// import type { AppProps } from "next/app";
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />;
-// }
 import type { AppProps } from "next/app";
 import Layout from "../components/layouts/Layout";
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
